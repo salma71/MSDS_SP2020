a81cb2f2da882880983bc283a489b1b02106d354
Salma Elshahawy <salmaeng71@gmail.com>
2020-01-29 16:42:24 -0500

    fix matrix case pivot zero

--- Spring2020/computational_mod_605/Week_1/Untitled.R
+++ Spring2020/computational_mod_605/Week_1/Untitled.R
@@ -1,22 +1,28 @@
 calculate_gauss <- function(a, b) {
   n <- nrow(a)
+  m <- ncol(a)
   # det <- 1
   # computes the array of col maximal elements
   # loop over columns to get the max element in col
-  for (i in seq_len(n - 1)) {
+  for (i in 1:n) {
     j <- which.max(a[i:n, i]) + i - 1
-    if (j != i) {
+    if (a[i, i] == 0) {
       a[c(i, j), i:n] <- a[c(j, i), i:n]
       b[c(i, j), ] <- b[c(j, i), ]
     }
+  
     # finds the multiplier to eliminate
-    k <- seq(i + 1, n)
-    for (j in k) {
-      s <- a[[j, i]] / a[[i, i]] 
-      a[j, k] <- a[j, k] - s * a[i, k]
-      # do the same for the other vector
-      b[j, ] <- b[j, ] - s * b[i, ]
-    }
+    # if(a[j,i] != 0) {
+      # k <- seq(i + 1, n)
+      for (k in 2:m) {
+          if(a[k,i] != 0 & k > i) {
+            s <- a[k, i] / a[i, i] 
+            a[k, ] <- a[k, ] - s * a[i, ]
+            # do the same for the other vector
+            b[k, ] <- b[k, ] - s * b[i, ]
+          }
+        }
+    # }
   }
   
   # do the same for rows
@@ -31,7 +37,7 @@ calculate_gauss <- function(a, b) {
   
   return(x=b)
 }
-a <- matrix(c(1,2,-1,1,-1,-2,3,5,4), nrow = 3, ncol = 3)
+a <- matrix(c(0,2,-1,1,-1,-2,3,5,4), nrow = 3, ncol = 3)
 b <- matrix(c(1,2,6), nrow = 3, ncol = 1)
 
 val <- calculate_gauss(a,b)
